33292680aafdcc3dee41d55722610ba5f03e4092
Jay Janarthanan <jay@jayonsoftware.com>
2019-03-22 05:55:13 -0400

    First Check in

---
+++ DemoAspNetCore/DL/CustomerDataLogic.cs
@@ -0,0 +1,88 @@
+﻿using DemoAspNetCore.Model;
+using System;
+using System.Collections.Generic;
+
+namespace DemoAspNetCore.DL
+{
+    public class CustomerDataLogic
+    {
+        // Get All Records
+        public List<Customer> GetAll()
+        {
+            var customerList = new List<Customer>();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                var customer = new Customer
+                {
+                    Id = i,
+                    Price = 3.33m,
+                    Email = "jay@jay.com",
+                    ReleaseDate = DateTime.Today,
+                    Title = "Demo"
+                };
+
+                customerList.Add(customer);
+            }
+
+            return customerList;
+        }
+
+
+        // Get a record by Id
+        public Customer Get(int id)
+        {
+            Console.WriteLine("Get All got called");
+            var customer = new Customer
+            {
+                Id = id,
+                Price = 3.33m,
+                Email = "jay@jay.com",
+                ReleaseDate = DateTime.Today,
+                Title = "The Id Is " + id
+            };
+
+            return customer;
+        }
+
+        // Delete
+        public bool Delete(int id)
+        {
+            Console.WriteLine("Delete got called");
+            return true;
+        }
+
+        // Save
+        public Customer Post(Customer dto)
+        {
+            Console.WriteLine("Post got called");
+            var customer = new Customer
+            {
+                Id = 1,
+                Price = dto.Price,
+                Email = dto.Email,
+                ReleaseDate = dto.ReleaseDate,
+                Title = dto.Title
+            };
+
+            return customer;
+        }
+
+        // Update
+        public Customer Put(Customer dto)
+        {
+            Console.WriteLine("Update Got Called");
+            var customer = new Customer
+            {
+                Id = dto.Id,
+                Price = dto.Price,
+                Email = dto.Email,
+                ReleaseDate = dto.ReleaseDate,
+                Title = dto.Title
+            };
+
+            return customer;
+        }
+
+    }
+}
---
+++ DemoAspNetCore/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoAspNetCore.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
