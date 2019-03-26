e0844743ec1ef1aa3bbcd9d322efc993581f8bca
pushist1y <mister.p.sh@gmail.com>
2019-03-27 00:31:53 +0300

    api and excel

--- DemoAspNetCore/Controllers/Api/DataController.cs
+++ DemoAspNetCore/Controllers/Api/DataController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DemoAspNetCore.DL;
 using DemoAspNetCore.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,52 @@ namespace DemoAspNetCore.Controllers.Api
         {
             var data = _dataProvider.GetAll(id);
             return new DataResult<CallListDto> {Data = data, Total = data.Count};
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<CallListDto> GetOne(int id)
+        {
+            var data = _dataProvider.Get(id);
+            return data;
+        }
+
+        [HttpPost]
+        public ActionResult<CallListDto> Create([FromForm] CallListDto input)
+        {
+            try
+            {
+                return _dataProvider.Post(input);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<CallListDto> Edit([FromForm] CallListDto input)
+        {
+            try
+            {
+                return _dataProvider.Put(input);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<bool> Delete(int id)
+        {
+            try
+            {
+                return _dataProvider.Delete(id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 
--- DemoAspNetCore/Typescript/CallList/CallList.Index.ts
+++ DemoAspNetCore/Typescript/CallList/CallList.Index.ts
@@ -20,6 +20,14 @@ export class CallListIndex {
     gridOptions.dataSource = new kendo.data.DataSource(dsOptions);
 
     this.grid = $("#dataGrid").empty().kendoGrid(gridOptions).data("kendoGrid");
+
+    //$(".k-grid-Excel").click(e => { this.excelButtonClick(e as any); });
+  }
+
+  excelButtonClick(e: JQueryEventObject) {
+    e.preventDefault();
+    this.grid.saveAsExcel();
+    console.log("excel export");
   }
 
   getGridOptions(): kendo.ui.GridOptions {
@@ -31,19 +39,27 @@ export class CallListIndex {
       },
       sortable: true,
       resizable: true,
+
+      batch: true,
+      editable: "inline",
       //selectable: "row",
       pageable: {
         pageSize: 10
       },
-      toolbar: ["create"],
+      toolbar: ["create", "excel" /*{ iconClass: "k-icon k-i-table-column-insert-right", text: "Excel"} as kendo.ui.GridToolbarItem*/],
       columns: [
-        { field: "id", title: "Id" },
+        { field: "id", title: "Id", editable: () => false, aggregates: ["count"], footerTemplate: "Total Count: #=count#", groupFooterTemplate: "Count: #=count#" },
         { field: "status", title: "Status" },
         { field: "name", title: "Name" },
-        { field: "callStartDate", format: "{0:dd.MM.yyyy}", title: "Call start date" },
+        { field: "callStartDate", format: "{0:dd.MM.yyyy}", title: "Call start date"},
         { field: "lastCallDate", format: "{0:dd.MM.yyyy}", title: "Last call date" },
-        { title: "Call sheet", template: "<a href=\"#: callSheet #\">#: callSheetLabel #</a>" }
-      ] as kendo.ui.TreeListColumn[]
+        { title: "Call sheet", template: "<a href=\"#: callSheet #\">#: callSheetLabel #</a>" },
+        { command: ["edit", "destroy"] }
+      ] as kendo.ui.TreeListColumn[],
+      excel: {
+        allPages: true,
+        fileName: "Calls.xlsx"
+      },
     } as kendo.ui.GridOptions;
   }
 
@@ -55,8 +71,45 @@ export class CallListIndex {
           url: `/api/data/index/${this.pageId}`,
           dataType: "json",
           type: "GET"
+        },
+        create: {
+          url: "/api/data",
+          type: "POST",
+          data: data => {
+            data.callStartDate = data.callStartDate.toISOString();
+            data.lastCallDate = data.lastCallDate.toISOString();
+            return data;
+          }
+        },
+        destroy: {
+          url: (o) => {
+            return `/api/data/${o.id}`;
+          },
+          dataType: "json",
+          type: "DELETE"
+        },
+        update: {
+          url: `/api/data`,
+          type: "PUT",
+          data: data => {
+            data.callStartDate = data.callStartDate.toISOString();
+            data.lastCallDate = data.lastCallDate.toISOString();
+            return data;
+          }
+        },
+        parameterMap: (options, operation) => {
+          console.log("Sending request");
+          console.log(operation);
+          console.log(options);
+
+          if (operation !== "read" && options.models) {
+            return { models: kendo.stringify(options.models) };
+          }
+          return null;
         }
+
       },
+
       serverPaging: false,
       serverFiltering: false,
       serverSorting: false,
@@ -76,9 +129,22 @@ export class CallListIndex {
             "callSheetLabel": { type: "string" },
             "callSheet": { type: "string" }
           }
-        }
+        },
+
       },
-      pageSize: 10
+      pageSize: 10,
+      error: (e) => {
+        let text = e.errorThrown + " " + e.xhr.responseText;
+        notifyError(text);
+        console.error(e);
+      },
+      group: {
+        field: "name",
+        aggregates: [{ field: "id", aggregate: "count" }, { field: "callStartDate", aggregate: "min" }, { field: "lastCallDate", aggregate: "max" }]
+      },
+      aggregate: [{ field: "id", aggregate: "count" }]
+      
+
     } as kendo.data.DataSourceOptions;
   }
 
@@ -89,4 +155,22 @@ export class CallListIndex {
       throw new Error('The kendo.aspnetmvc.min.js script is not included.');
     }
   }
+}
+
+var notificationElement: JQuery;
+var popupNotification: kendo.ui.Notification;
+
+export function notifyError(errorText: string) {
+  if (!popupNotification) {
+    notificationElement = $("span");
+    popupNotification = notificationElement.kendoNotification({
+      stacking: "down",
+      position: {
+        top: 30,
+        right: 30
+      }
+    } as kendo.ui.NotificationOptions).data("kendoNotification");
+  }
+
+  popupNotification.show(errorText, "error");
 }
